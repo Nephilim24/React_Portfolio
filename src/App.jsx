9a19efc41778d11e103a7a32b8a82edae9c1793c
Nephilim24 <muratacc21@gmail.com>
2025-10-16 21:47:07 +0500

    Trying to add Vercel Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header.jsx";
 import Main from "./components/Main/Main.jsx";
 import Loader from "./components/Loader/Loader.jsx";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Loader />
       <Header />
       <Main />
+      <Analytics />
     </>
   );
 }
